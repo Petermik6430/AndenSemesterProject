81cfcff4abc0cccc37b80bb875737fff249b7123
AliBarakji <167064603+AliBarakji@users.noreply.github.com>
2024-12-03 13:29:17 +0100

    Update CustomerController.java

--- AndetSemesterProjekt/src/controller/CustomerController.java
+++ AndetSemesterProjekt/src/controller/CustomerController.java
@@ -4,6 +4,7 @@ import db.CustomerDB;
 import db.CustomerDBIF;
 import db.DataAccessException;
 import model.Customer;
+import model.Employee;
 
 public class CustomerController {
 	
@@ -23,43 +24,17 @@ public class CustomerController {
 	}
 	
 
-
-/*
 	// fortæller hvad vi skal kunne gøre med databasen gemme kunde og finde en kunde baseret på telefonnumnmer
 	public interface CustomerDBIF {
 	    void saveCustomer(Customer customer);
 	    Customer getCustomerByPhone(String phoneNo);
 	}
-	*/
 	
-	
-/*
-public class customerController { 
-	private customerDBIF customerDB; 
-	
-	public customerController(customerDBIF customerDB) { 
-		this.customerDB = customerDB; 
-		}
-
-	 
-	
-/*	
-	 public void createCustomer(int customerId, String fName, String lName, String phoneNo, String mail) {
-	        Customer newCustomer = new Customer(customerId, fName, lName, phoneNo, mail);
-	        customerDB.saveCustomer(newCustomer); // Kalder databasegrænsefladen for at gemme kunden
-	       
-	 } 
-	 
-	// findCustomerByPhoneNo-metoden, der finder en kunde via telefonnummer
-	    public Customer findCustomerByPhoneNo(String phoneNo) {
-	        Customer customer = customerDB.getCustomerByPhone(phoneNo); // Kalder databasegrænsefladen for at hente kunden
-	        if (customer != null) {
-	            System.out.println("Kunde fundet: " + customer.getfName() + " " + customer.getlName());
-	        } else {
-	            System.out.println("Ingen kunde fundet med telefonnummeret: " + phoneNo);
-	       
-	        return customer;
-	    }
-	
+	public Customer findCustomerByPhoneNo(String phoneNo) throws DataAccessExeption {
+		Customer customer = customerDB.findCustomerByPhoneNo(CustomerPhoneNo); 
+		return CustomerDB.findCustomerByPhoneNo(phoneNo); 
 	}
-		
+	
+	
+	
+}
