f231d29b31bc89039b095bd81e5f42e32f6d9447
Petermik6430 <peter.mikkelsen@hamso.dk>
2024-12-06 10:35:24 +0100

    Create DbConnectionTest.java

---
+++ AndetSemesterProjekt/test/db/DbConnectionTest.java
@@ -0,0 +1,23 @@
+package db;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.sql.Connection;
+
+import db.DBConnection;
+
+public class DbConnectionTest {
+
+	
+	@Test
+	public void testGetConnection() {
+
+		try {
+			Connection c = DBConnection.getInstance().getConnection();
+			assertNotNull(c);
+		} catch (Exception e) {
+			fail("issues with dbconnection");
+		}
+	}
+
+}
